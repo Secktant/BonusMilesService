29426757d8c43f1788e265e252a7c00edf09c3ee
Nikita Santalov <na.santalov@yandex.ru>
2022-03-22 22:04:27 +0400

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int miles = service.calculate(10_000);
+        int price = 10_000;
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
